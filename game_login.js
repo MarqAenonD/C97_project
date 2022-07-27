44a595aa8129e1c4ad55443c4777afdb0b58ea77
MarqAenonD <106565814+MarqAenonD@users.noreply.github.com>
2022-07-27 17:09:20 -0500

    Update game_login.js

--- game_login.js
+++ game_login.js
@@ -5,8 +5,8 @@ function addUser() {
   player2_name = document.getElementById("player2_name_input").value;
 
   // Store these values locally
-  localStorage.setItem(player1_name);
-  localStorage.setItem(player2_name);
+  localStorage.setItem("player1_name",player1_name);
+  localStorage.setItem("player2_name",player2_name);
 
   //Assign "game_page.html" to window.location
   window.location = "game_page.html";
